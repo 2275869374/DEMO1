424f0621fdf9330f4142234b43a32087413f3418
2275869374 <2275869374@qq.com>
2020-05-07 14:45:52 +0800

    first test

--- Dedup.java
+++ Dedup.java
@@ -52,7 +52,7 @@ public static void main(String[] args) throws Exception{
 	job.setOutputKeyClass(Text.class);      
 	
 	job.setOutputValueClass(Text.class); 
-
+	修改	
        //设置输入和输出目录      
 	FileInputFormat.addInputPath(job,new Path(otherArgs[0]));   
 	FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));      
